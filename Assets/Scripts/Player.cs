1c216fdc889edff4e74c97ff1321ce68ec343320
flslin <84002788+flslin@users.noreply.github.com>
2024-04-01 11:12:12 +0900

    플레이어 좌우이동

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // 스피드
+    public float moveSpeed = 3;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        // x값 설정 vector 방향 * 시간 * 스피드
+        float distanceX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+
+        // x 이동 설정
+        transform.Translate(distanceX, 0, 0);
+    }
+}
